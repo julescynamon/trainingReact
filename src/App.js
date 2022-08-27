4872ce6e52a931261bcf51d4a9f8b85531ad0f1c
cynamon jules <julescweb@mail.com>
2022-08-27 10:55:40 +0200

    projet finis

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Homepage from "./pages/Homepage/Homepage";
+import Admin from "./pages/Admin/Admin";
+import styles from "./App.module.scss";
+// import { seedRecipes } from "./data/seed";
+
+// seedRecipes();
+
+function App() {
+  const [page, setPage] = useState("homepage");
+
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header setPage={setPage} />
+      {page === "homepage" && <Homepage />}
+      {page === "admin" && <Admin />}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import styles from "./Header.module.scss";
+import cookchef from "../../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "./components/HeaderMenu/HeaderMenu";
+
+function Header({ setPage }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <div className="flex-fill">
+        <img
+          onClick={() => setPage("homepage")}
+          src={cookchef}
+          alt="logo cookchef"
+        />
+      </div>
+      <ul className={styles.headerList}>
+        <button
+          onClick={() => setPage("admin")}
+          className="btn btn-primary mr-15"
+        >
+          Ajouter une recette
+        </button>
+        <button className="mr-15 btn btn-reverse-primary">
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
+        </button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu setPage={setPage} />
+        </>
+      )}
+    </header>
+  );
+}
+
+export default Header;
